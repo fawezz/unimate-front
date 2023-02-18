145decc09fc339c22b7712dddabb614c4556190c
faouez <faouez.marzouk@esprit.tn>
2023-02-18 03:01:48 +0100

    created routes file
    a file that groups all routes' names

---
+++ lib/app/Routes.dart
@@ -0,0 +1,7 @@
+class NamedRoutes {
+  static const String login = '/login';
+  static const String signUp = '/signUp';
+  static const String forgetPwdEmail = '/forgetPwdEmail';
+  static const String forgetPwdOTP = '/otp';
+  static const String resetPwd = '/resetPwd';
+}
